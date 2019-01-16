d31e998fa33664676dd33f9ef1f04789a94bcaa8
Overlander <Dan_Overlander@Dell.com>
2019-01-16 14:46:54 -0600

    init-Deploying distributed systems

--- Databases/create_databases.sql
+++ Databases/create_databases.sql
@@ -10,8 +10,6 @@ DROP DATABASE IF EXISTS tracker_timesheets_dotnet_test;
 CREATE USER IF NOT EXISTS 'tracker_dotnet'@'localhost'
   IDENTIFIED BY 'password';
 
-GRANT ALL PRIVILEGES ON *.* TO 'tracker_dotnet' @'localhost';
-
 CREATE DATABASE tracker_allocations_dotnet_dev;
 CREATE DATABASE tracker_backlog_dotnet_dev;
 CREATE DATABASE tracker_registration_dotnet_dev;
